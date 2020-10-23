5c7ef224271bf447d428dd019fa72f2ed867f083
cokoghenun <confidenceboi@gmail.com>
2020-10-23 09:34:50 +0000

    chore: First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const server = require('express')();
+const PORT = process.env.PORT;
+
+server.get('/', (req, res) => {
+  const text = `Welcome to Test App \n  Your env secret is ${process.env.SECRET}`;
+  res.send(text);
+});
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server on http://localhost:${PORT}/`);
+});
